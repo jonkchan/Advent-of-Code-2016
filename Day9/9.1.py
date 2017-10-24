a3c76f2d8578e6f0b7e3e8bb8a8039d88bb94dcf
Jonathan Chan <johnkchan@users.noreply.github.com>
2017-10-24 16:53:40 -0400

    Update 9.1.py

--- Day9/9.1.py
+++ Day9/9.1.py
@@ -1,2 +1,18 @@
-# Puzzle Input
-key = ""
+key = '''A(1x5)BC'''
+output = ""
+
+for index, item in enumerate(key):
+    switch = 0
+    if switch != 0:
+        pass
+    else:
+        if item != "(":
+            output += item
+        else:
+            length = int(key[index + 1])
+            mult = int(key[index + 3])
+            print(length, mult)
+            output += key[index + 5 * length] * mult
+            switch = length
+        switch -= 1
+print(output)
